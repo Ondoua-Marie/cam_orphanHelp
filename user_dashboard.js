d31d932726559ac43a9e3f71b41fb95660136582
elwvll <elwillisz058@gmail.com>
2026-01-09 18:56:19 -0800

    Add files via upload

---
+++ user_dashboard.js
@@ -0,0 +1,7 @@
+// Future database logic will go here
+console.log("Dashboard loaded successfully");
+
+// Example action
+document.querySelector(".donate-btn").addEventListener("click", () => {
+    alert("Redirecting to Donate Page...");
+});
